857f225405f86082ad9538cb72a87535db2e7939
khemx3 <phakapol_chuliang@yahoo.com>
2019-04-25 19:01:07 +0700

    Circle python

---
+++ LAB2/CircleTest.py
@@ -0,0 +1,50 @@
+import unittest
+from Circle import *
+
+
+class CircleTest(unittest.TestCase):
+
+    knownValues = [(1,6.283185307179586,3.141592653589793),
+                (2,12.566370614359172,12.566370614359172),
+                (3,18.84955592153876,28.274333882308138),
+                (4,25.132741228718345,50.26548245743669),
+                (5,31.41592653589793,78.53981633974483),
+              
+    ]
+
+    knownXY = [(1, 1, 1, 1),
+            (2, 2, 2, 2)
+            ]
+    def setUp(self):
+        self.c = Circle()
+
+    def testForParameter(self):
+        for x, y, z in self.knownValues:
+            self.c.setRadius(x)
+            self.assertEqual(self.c.getPerimeter(), y)
+            self.assertEqual(self.c.getArea(), z)
+
+    def testZeroRadius(self):
+        for x, y, z in self.knownValues:
+            self.assertRaises(InvalidInputError, self.c.setRadius(x), 0)
+    
+    def testNegativeRadius(self):
+        for x, y, z in self.knownValues:
+            self.assertRaises(InvalidInputError, self.c.setRadius(x), -1)
+
+    def testNewXY(self):
+        for a, b, c, d in self.knownXY:
+            self.assertEqual(self.c.move(a, b), (c, d))
+    
+    def testIntInput(self):
+        for a, b, c, d in self.knownXY:
+            self.assertEqual(type(self.c.getX()), int)
+            self.assertEqual(type(self.c.getY()), int)
+            
+                
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
--- LAB2/circle.py
+++ LAB2/circle.py
@@ -1,16 +1,39 @@
 import math
+
+class StackError(Exception): pass
+class InvalidInputError(StackError): pass
+
 class Circle:
-    def __init__(self , x=0, y=0, radius):
+
+    def __init__(self , x=0, y=0, radius=1):
         self.x = x
         self.y = y
         self.radius = radius
     
+    def setRadius(self, val):
+        if val <= 0:
+            raise InvalidInputError
+        else:
+            self.radius = float(val)
+
     def getArea(self):
         area = math.pi * (self.radius) ** 2
         return area 
+
     def getPerimeter(self) :
         return 2 * math.pi * self.radius
+
     def move(self, x , y):
-        self.x = x
-        self.y = y
-        return x, y
+            self.x = x
+            self.y = y
+            return x, y
+
+    def getX(self):
+        return self.x
+        
+    def getY(self):
+        return self.y
+
+
+
+        
